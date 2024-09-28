39beae0333a5b3ad5cc61379850e5b2845d0369e
Patrick Schmitt <patrick_98@t-online.de>
2024-09-28 18:52:51 +0200

    Use openai/whisper-large-v3  (#2)
    * major improvement: use openai/whisper-large-v3 for transcription and translation

--- README.md
+++ README.md
@@ -1,30 +1,28 @@
 # Subtitle Creator
 
 This scripts creates english subtitles for an input video.
-It works completely locally using [Vosk](https://github.com/alphacep/vosk-api) for transcription
-and [Argos](https://github.com/argosopentech/argos-translate) for translation.
+It works completely locally using [openai/whisper-large-v3](https://huggingface.co/openai/whisper-large-v3) for transcription
+and translation.
 
 ## Requirements
 
-- Audio extracting and adding the subtitles requires [FFmpeg](https://ffmpeg.org/).
-- Transcription of the original video requires the corresponding [Vosk Model](https://alphacephei.com/vosk/models) for
-  the video's language.
+- Audio extracting and adding the subtitles to the video requires [FFmpeg](https://ffmpeg.org/).
 - Python dependencies are managed via [Poetry](https://python-poetry.org/): ``poetry install``
+- It is recommended to install [CUDA](https://developer.nvidia.com/cuda-downloads) for faster transcription and translation.
 
 ## Usage
 
 Start a shell in the virtual env via ``poetry shell``, then run the script with:
-``python subtitleCreator.py <input video> <vosk model>``.
+``python subtitleCreator.py <input video>``.
 Full options: ``python subtitleCreator.py -h``:
 
 ````text
-usage: subtitleCreator.py [-h] [-o OUTPUT] [--overwrite] video_file vosk_model_path
+usage: subtitleCreator.py [-h] [-o OUTPUT] [--overwrite] video_file 
 
 Add translated subtitles to a video.
 
 positional arguments:
   video_file            Path to the input video file.
-  vosk_model_path       Path to the Vosk model directory.
 
 options:
   -h, --help            show this help message and exit
--- subtitleCreator.py
+++ subtitleCreator.py
@@ -1,35 +1,18 @@
 import argparse
-import datetime
-import json
 import os
 import shutil
 import subprocess
-import wave
+import datetime
+
 import srt
-
-from argostranslate import package, translate
-from langdetect import detect, DetectorFactory
-from vosk import Model, KaldiRecognizer
-
-# Set seed to make langdetect results reproducible
-DetectorFactory.seed = 0
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 
 def check_ffmpeg():
     """Check if FFmpeg is installed, otherwise raise an error."""
     if not shutil.which("ffmpeg"):
         raise EnvironmentError("FFmpeg is not installed. Please install it to use this script.")
-
-
-def install_argos_model(source_lang, target_lang='en'):
-    """Install the Argos Translate model for the specified source and target languages."""
-    available_packages = package.get_available_packages()
-    for pkg in available_packages:
-        if pkg.from_code == source_lang and pkg.to_code == target_lang:
-            package.install_from_path(pkg.download())
-            print(f"Installed {pkg} for translation from {source_lang} to {target_lang}.")
-            return True
-    raise ValueError(f"No Argos Translate model available for {source_lang} to {target_lang}.")
 
 
 def extract_audio(video_file, audio_file):
@@ -44,91 +27,63 @@ def extract_audio(video_file, audio_file):
         raise e
 
 
-def transcribe_audio(audio_file: str, vosk_model_path: str) -> list[srt.Subtitle]:
+def translate_audio(audio_file: str) -> list[dict]:
+    """Translate the audio from a file using openai/whisper-large-v3 model.
+
+    Args:
+        audio_file (str): Path to the audio file
+
+    Returns:
+        list[dict]: List of translated chunks with timestamps
     """
-    Transcribe audio using Vosk.
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-    :param audio_file: Path to the audio file to be transcribed
-    :param vosk_model_path: Path to the Vosk model file
-    :return: A list of transcription results in JSON format, each represented as a SRT subtitle object
-    """
-    # Open the audio file as a wave object
-    wf = wave.open(audio_file, 'rb')
+    model_id = "openai/whisper-large-v3"
 
-    # Initialize the Vosk model and recognizer with the provided model path and audio file parameters
-    model = Model(vosk_model_path)
-    recognizer = KaldiRecognizer(model, wf.getframerate())
-    recognizer.SetWords(True)
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+    model.to(device)
 
-    results = []
+    print(f"Creating processor for model {model_id}")
+    processor = AutoProcessor.from_pretrained(model_id)
 
-    while True:
-        data = wf.readframes(4000)
-        if len(data) == 0:
-            break
-        if recognizer.AcceptWaveform(data):
-            results.append(recognizer.Result())
-    results.append(recognizer.FinalResult())
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        torch_dtype=torch_dtype,
+        chunk_length_s=30,
+        batch_size=16,  # batch size for inference - set based on your device
+        device=device,
+    )
 
+    # get the result with timestamps
+    print("Starting translation and transcription")
+    result = pipe(audio_file, return_timestamps=True, generate_kwargs={"task": "translate"})
+    print("Finished translation and transcription")
+    return result["chunks"]
+
+
+def create_subs(translations: list[dict]):
+    """Create a list of Subtitle objects for each translation chunk."""
+
+    # TODO: handle duplicate consecutive translations and add them only once
     subs = []
-    words_per_line = 7
-    for res in results:
-        jres = json.loads(res)
-        if not "result" in jres:
-            continue
-        words = jres["result"]
-        for j in range(0, len(words), words_per_line):
-            line = words[j: j + words_per_line]
-            s = srt.Subtitle(index=len(subs),
-                             content=" ".join([l["word"] for l in line]),
-                             start=datetime.timedelta(seconds=line[0]["start"]),
-                             end=datetime.timedelta(seconds=line[-1]["end"]))
-            subs.append(s)
+    for i in range(0, len(translations)):
+        translation = translations[i]
+        timestamp = translation['timestamp']
+        text = translation['text']
+        s = srt.Subtitle(index=i, content=text, start=datetime.timedelta(seconds=timestamp[0]), end=datetime.timedelta(seconds=timestamp[1]))
+        subs.append(s)
 
-
-
-    wf.close()  # Close the wave object
     return subs
 
 
-def detect_language(transcriptions: list[srt.Subtitle]) -> str:
-    """Detect the source language of a video by looking at the first few transcription results."""
-    sample_text = ' '.join(sub.content for sub in transcriptions[:10])
-    detected_lang = detect(sample_text)
-    return detected_lang
 
-
-def translate_transcription(transcriptions: list[srt.Subtitle], source_lang_code) -> list[srt.Subtitle]:
-    """
-    Translate transcriptions to English using Argos Translate.
-
-    :param transcriptions: A list of SRT subtitle objects containing the transcription results
-    :param source_lang_code: The language code for the source language (e.g. 'en', 'es', etc.)
-    :return: A list of translated SRT subtitle objects in the target language ('en')
-    """
-    installed_languages = translate.get_installed_languages()
-
-    # Check if a translation model is available for the specified source language
-    if not any(lang.code == source_lang_code for lang in installed_languages):
-        raise ValueError(f"No Argos Translate model available for {source_lang_code}.")
-
-    # Get the translator object for the specified source and target languages
-    source_lang = next(lang for lang in installed_languages if lang.code == source_lang_code)
-    target_lang = next(lang for lang in installed_languages if lang.code == 'en')
-    translator = source_lang.get_translation(target_lang)
-
-    # Translate each transcription result using the Argos Translate model
-    translations = []
-    for result in transcriptions:
-        text = result.content
-        translated = translator.translate(text)
-        subs_clone = srt.Subtitle(index=result.index, content=translated, start=result.start, end=result.end)
-        translations.append(subs_clone)
-
-    return translations
-
-
-def create_srt(subs: list[srt.Subtitle], srt_file: str):
+def create_srt_files(subs: list[srt.Subtitle], srt_file: str):
     """
     Create SRT file from transcriptions and translations.
 
@@ -140,9 +95,8 @@ def create_srt(subs: list[srt.Subtitle], srt_file: str):
         raise ValueError("The provided path should point to an empty file with a '.srt' extension.")
 
     with open(srt_file, 'w', encoding='utf-8') as f:
-       f.write(srt.compose(subs))
-       f.close()
-
+        f.write(srt.compose(subs))
+        f.close()
 
 
 def add_selectable_subtitles_to_video(video_file, subtitle_file, output_video):
@@ -171,23 +125,24 @@ def add_selectable_subtitles_to_video(video_file, subtitle_file, output_video):
 
 
 # Main Workflow
-def main(video_file, vosk_model_path, output_file, overwrite):
+def main(video_file, output_file, overwrite):
     check_ffmpeg()
 
+    # temp files. should be configurable in the future and probably be deleted after processing is done.
     audio_file = 'audio.wav'
     subtitle_file = 'subtitles.srt'
 
     extract_audio(video_file, audio_file)
-    transcriptions = transcribe_audio(audio_file, vosk_model_path)
-    detected_lang_code = detect_language(transcriptions)
 
-    print(f"Detected language code: {detected_lang_code}")
 
-    # Install Argos Translate model based on detected language
-    install_argos_model(detected_lang_code)
+    # transcribe audio
+    translations = translate_audio(audio_file)
 
-    translations = translate_transcription(transcriptions, detected_lang_code)
-    create_srt(translations, subtitle_file)
+   # create subtitles from translations
+    subs = create_subs(translations)
+
+    # Create Subtitle file
+    create_srt_files(subs, subtitle_file)
 
     # Determine output file path
     if not output_file:
@@ -200,20 +155,19 @@ def main(video_file, vosk_model_path, output_file, overwrite):
                 f"Output file '{output_file}' already exists. Use the --overwrite flag to overwrite it.")
         output_video = output_file
 
+    # Add selectable subtitles to video
     add_selectable_subtitles_to_video(video_file, subtitle_file, output_video)
     print(f"Selectable subtitles added successfully to the video! Output saved to: {output_video}")
-
 
 
 # Command-line arguments setup
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Add translated subtitles to a video.")
     parser.add_argument("video_file", help="Path to the input video file.")
-    parser.add_argument("vosk_model_path", help="Path to the Vosk model directory.")
     parser.add_argument("-o", "--output",
                         help="Path to the output video file. If not specified the video will be saved as "
                              "'<input_file>_subtitles.mp4'.")
     parser.add_argument("--overwrite", help="Overwrite the original video file.", action="store_true")
     args = parser.parse_args()
 
-    main(args.video_file, args.vosk_model_path, args.output, args.overwrite)
+    main(args.video_file, args.output, args.overwrite)
