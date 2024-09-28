ae65b40277a27e45359bd6d095ec7a1d99f105fa
Zuplyx <patrick_98@t-online.de>
2024-09-28 15:19:14 +0200

    improve transcription and ffmpeg handling:
    - transcribe directly to srt.Subtitle and use the srt library to create the subtitle file
    - fixed ffmpeg arguments so subtitles are added correctly (fixed mapping)

--- subtitleCreator.py
+++ subtitleCreator.py
@@ -1,10 +1,11 @@
 import argparse
+import datetime
 import json
 import os
 import shutil
 import subprocess
 import wave
-from datetime import timedelta
+import srt
 
 from argostranslate import package, translate
 from langdetect import detect, DetectorFactory
@@ -43,13 +44,13 @@ def extract_audio(video_file, audio_file):
         raise e
 
 
-def transcribe_audio(audio_file, vosk_model_path):
+def transcribe_audio(audio_file: str, vosk_model_path: str) -> list[srt.Subtitle]:
     """
     Transcribe audio using Vosk.
 
     :param audio_file: Path to the audio file to be transcribed
     :param vosk_model_path: Path to the Vosk model file
-    :return: A list of transcription results in JSON format
+    :return: A list of transcription results in JSON format, each represented as a SRT subtitle object
     """
     # Open the audio file as a wave object
     wf = wave.open(audio_file, 'rb')
@@ -63,65 +64,84 @@ def transcribe_audio(audio_file, vosk_model_path):
 
     while True:
         data = wf.readframes(4000)
-        if len(data) == 0:  # If no more data to read, break the loop
+        if len(data) == 0:
             break
         if recognizer.AcceptWaveform(data):
-            results.append(json.loads(recognizer.Result()))
-    results.append(json.loads(recognizer.FinalResult()))
+            results.append(recognizer.Result())
+    results.append(recognizer.FinalResult())
 
-    # Add the final transcription result to the list
-    results.append(json.loads(recognizer.FinalResult()))
+    subs = []
+    words_per_line = 7
+    for res in results:
+        jres = json.loads(res)
+        if not "result" in jres:
+            continue
+        words = jres["result"]
+        for j in range(0, len(words), words_per_line):
+            line = words[j: j + words_per_line]
+            s = srt.Subtitle(index=len(subs),
+                             content=" ".join([l["word"] for l in line]),
+                             start=datetime.timedelta(seconds=line[0]["start"]),
+                             end=datetime.timedelta(seconds=line[-1]["end"]))
+            subs.append(s)
+
+
+
     wf.close()  # Close the wave object
-    return results
+    return subs
 
 
-def detect_language(transcriptions):
+def detect_language(transcriptions: list[srt.Subtitle]) -> str:
     """Detect the source language of a video by looking at the first few transcription results."""
-    sample_text = ' '.join(result['text'] for result in transcriptions if 'text' in result)
+    sample_text = ' '.join(sub.content for sub in transcriptions[:10])
     detected_lang = detect(sample_text)
     return detected_lang
 
 
-def translate_transcription(transcriptions, source_lang_code):
-    """Translate transcriptions to English using Argos Translate."""
+def translate_transcription(transcriptions: list[srt.Subtitle], source_lang_code) -> list[srt.Subtitle]:
+    """
+    Translate transcriptions to English using Argos Translate.
+
+    :param transcriptions: A list of SRT subtitle objects containing the transcription results
+    :param source_lang_code: The language code for the source language (e.g. 'en', 'es', etc.)
+    :return: A list of translated SRT subtitle objects in the target language ('en')
+    """
     installed_languages = translate.get_installed_languages()
+
+    # Check if a translation model is available for the specified source language
+    if not any(lang.code == source_lang_code for lang in installed_languages):
+        raise ValueError(f"No Argos Translate model available for {source_lang_code}.")
+
+    # Get the translator object for the specified source and target languages
     source_lang = next(lang for lang in installed_languages if lang.code == source_lang_code)
     target_lang = next(lang for lang in installed_languages if lang.code == 'en')
     translator = source_lang.get_translation(target_lang)
 
+    # Translate each transcription result using the Argos Translate model
     translations = []
     for result in transcriptions:
-        if 'text' in result:
-            text = result['text']
-            translated = translator.translate(text)
-            translations.append(translated)
+        text = result.content
+        translated = translator.translate(text)
+        subs_clone = srt.Subtitle(index=result.index, content=translated, start=result.start, end=result.end)
+        translations.append(subs_clone)
+
     return translations
 
 
-def seconds_to_timestamp(seconds):
-    """Convert seconds to SRT timestamp format"""
-    td = timedelta(seconds=seconds)
-    return str(td)
+def create_srt(subs: list[srt.Subtitle], srt_file: str):
+    """
+    Create SRT file from transcriptions and translations.
 
+    :param subs: List of SRT subtitle objects to be written into the SRT file.
+    :param srt_file: Path where the SRT file will be created. It should have a '.srt' extension.
+    :return: None
+    """
+    if not srt_file.endswith('.srt'):
+        raise ValueError("The provided path should point to an empty file with a '.srt' extension.")
 
-def create_srt(transcriptions, translations, srt_file):
-    """Create SRT file from transcriptions and translations."""
     with open(srt_file, 'w', encoding='utf-8') as f:
-        # remove empty entries
-        filtered_pairs = [
-            (transcription, translation)
-            for transcription, translation in zip(transcriptions, translations)
-            if transcription['text'] and translation.strip()
-        ]
-
-        for i, (transcription, translation) in enumerate(filtered_pairs):
-            if 'result' in transcription and transcription['result']:
-                start_time = seconds_to_timestamp(transcription['result'][0]['start'])
-                end_time = seconds_to_timestamp(transcription['result'][-1]['end'])
-                f.write(f"{i + 1}\n")
-                f.write(f"{start_time} --> {end_time}\n")
-                f.write(f"{translation}\n\n")
-        f.close()
+       f.write(srt.compose(subs))
+       f.close()
 
 
 
@@ -129,12 +149,18 @@ def add_selectable_subtitles_to_video(video_file, subtitle_file, output_video):
     """Add subtitle file to video using ffmpeg."""
     try:
         subprocess.run([
-            'ffmpeg', '-i', video_file, '-i', subtitle_file,
-            '-map', '0:v:0', '-map', '0:a:0', '-map', '1:s:0',
-            '-c:v', 'copy', '-c:a', 'copy', '-c:s', 'mov_text',
+            'ffmpeg',
+            '-i', video_file,
+            '-i', subtitle_file,
+            '-c:v', 'copy',
+            '-c:a', 'copy',
+            '-c:s', 'mov_text',
             '-metadata:s:s:0', 'language=eng',
-            '-shortest',
-            '-y', output_video
+            '-map', '0:v',
+            '-map', '0:a',
+            '-map', '1:0',
+            '-y',
+            output_video
         ], check=True)
     except subprocess.CalledProcessError as e:
         print(f"FFmpeg command failed: {e.returncode}. Check the logs for more information.")
@@ -161,7 +187,7 @@ def main(video_file, vosk_model_path, output_file, overwrite):
     install_argos_model(detected_lang_code)
 
     translations = translate_transcription(transcriptions, detected_lang_code)
-    create_srt(transcriptions, translations, subtitle_file)
+    create_srt(translations, subtitle_file)
 
     # Determine output file path
     if not output_file:
